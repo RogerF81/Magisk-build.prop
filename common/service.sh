dc3602284c866bac4e830b0b6cd83329a2ad1848
RogerF81 <lowhouse123@gmail.com>
2018-09-04 22:11:52 +0200

    Add some experimental props

--- common/service.sh
+++ common/service.sh
@@ -5,3 +5,9 @@ MODDIR=${0%/*}
 
 # This script will be executed in late_start service mode
 # More info in the main Magisk thread
+echo "af.fast_track_multiplier=4" >> /vendor/build.prop
+echo "vendor.audio.dolby.ds2.hardbypass=false" >> /vendor/build.prop
+echo "vendor.audio.offload.multiple.enabled=false" >> /vendor/build.prop
+echo "vendor.audio.offload.passthrough=false" >> /vendor/build.prop
+echo "ro.sys.fw.bg_apps_limit=72" >> /vendor/build.prop
+echo "ro.vendor.qti.sys.fw.bg_apps_limit=72" >> /vendor/build.prop
